8407c6d726aaf44e9d53c2177fa5e75b98ed46c2
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-12-03 17:02:30 +0200

    Complit Stop and Tree

---
+++ New Unity Project/Assets/Scripts/Stop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour {
+
+    private bool paused = false;
+
+    public void Pause()
+    {
+        if (!paused)
+        {
+            Time.timeScale = 0;
+            paused = true;
+           
+        }
+        else
+        {
+            Time.timeScale = 1;
+            paused = false;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
