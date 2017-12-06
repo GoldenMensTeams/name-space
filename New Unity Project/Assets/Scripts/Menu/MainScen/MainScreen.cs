9bb84b0de093d06f226122e752fe2e46ae96a497
daletavg <daletavg@gmail.com>
2017-12-07 00:20:22 +0200

    add yes no on main scen

---
+++ New Unity Project/Assets/Scripts/Menu/MainScen/MainScreen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScreen : MonoBehaviour {
+
+    GameObject acceptCanvas;
+    // Use this for initialization
+    private void Awake()
+    {
+        acceptCanvas = GameObject.FindGameObjectWithTag("AcceptCanvas");
+    }
+    void Start () {
+        SetDisActiveAcceptCanvas();
+    }
+    public void SetActiveAcceptCanvas()
+    {
+        acceptCanvas.SetActive(true);
+    }
+    public void SetDisActiveAcceptCanvas()
+    {
+        acceptCanvas.SetActive(false);
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    void AndroidBackButton()
+    {
+        if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.WindowsEditor)
+
+        {
+            Debug.Log("set platform");
+            if (Input.GetKeyDown(KeyCode.Home) || Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Menu))
+            {
+                if (acceptCanvas.active == true)
+                {
+                    SetDisActiveAcceptCanvas();
+                }
+                else
+                {
+                    SetActiveAcceptCanvas();
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        AndroidBackButton();
+    }
+}
